d4d95ea582e4c91c7a97f9e37d2d27eb88c0fa97
rumforweb <agaphonova.ea@gmail.com>
2023-07-30 13:42:27 +0300

    added buttons

--- src/assets/js/components/slider.js
+++ src/assets/js/components/slider.js
@@ -1,6 +1,22 @@
 var splide = new Splide( '.splide', {
   type   : 'loop',
-  arrows : false
+  arrows : false,
+  classes: {
+		pagination: 'splide__pagination dinner-pagination',
+		page      : 'splide__pagination__page dinner-page',
+  },
+} );
+
+
+splide.on( 'pagination:mounted', function ( data ) {
+  // You can add your class to the UL element
+  data.list.classList.add( 'splide__pagination--custom' );
+
+  // `items` contains all dot items
+  data.items.forEach( function ( item ) {
+    item.button.textContent = String( item.page + 1 );
+  } );
 } );
 
 splide.mount();
+
